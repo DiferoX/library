fad39b44c0fe2468aa632afec6e0be80a4fcd22f
Diego <diferox@gmail.com>
2022-07-09 01:39:04 -0500

    edit header and content class

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function openForm()
+{
+  let open = document.getElementById('formCont');
+  open.style.display = "block";
+}
+
+function closeForm()
+{
+  let close = document.getElementById('formCont');
+  close.style.display = "none";
+}
